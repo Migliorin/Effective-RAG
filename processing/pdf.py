92988d32576a7a9b2554744767e5d30a28eb8e18
Lucas Migliorin da Rosa <lucasmigliorin@hotmail.com>
2026-03-28 17:29:19 -0400

    feat: Preprocessing pdf into image tmp

---
+++ processing/pdf.py
@@ -0,0 +1,25 @@
+from tempfile import NamedTemporaryFile
+from PIL import Image
+import fitz
+
+class PDF():
+    def __init__(self):
+        pass
+
+    def extract_pages_into_imgs(self,path_pdf:str,zoom=2,delete=False)->list:
+        doc = fitz.open(path_pdf)
+        mat = fitz.Matrix(zoom,zoom)
+        list_paths = list()
+        for page in doc:
+            tmp_img_path = NamedTemporaryFile("w",suffix=".png",delete=delete)
+            pix = page.get_pixmap(matrix=mat)
+            Image.frombytes("RGB",[pix.width,pix.height],pix.samples).save(tmp_img_path.name)
+
+            list_paths.append(tmp_img_path.name)
+        return list_paths
+
+
+
+
+
+        
